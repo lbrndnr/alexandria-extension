da36d6aa591c9af3ea23e0390a3ffd9bdeb33430
Laurin Brandner <mail@laurinbrandner.ch>
2023-10-24 16:36:10 +0200

    Remove unnecessary files

--- Alexandria Extension/SafariExtensionHandler.swift
+++ Alexandria Extension/SafariExtensionHandler.swift
@@ -9,36 +9,5 @@ import SafariServices
 import os.log
 
 class SafariExtensionHandler: SFSafariExtensionHandler {
-
-    override func beginRequest(with context: NSExtensionContext) {
-        let request = context.inputItems.first as? NSExtensionItem
-
-        let profile: UUID?
-        if #available(iOS 17.0, macOS 14.0, *) {
-            profile = request?.userInfo?[SFExtensionProfileKey] as? UUID
-        } else {
-            profile = request?.userInfo?["profile"] as? UUID
-        }
-
-        os_log(.default, "The extension received a request for profile: %@", profile?.uuidString ?? "none")
-    }
-
-    override func messageReceived(withName messageName: String, from page: SFSafariPage, userInfo: [String : Any]?) {
-        page.getPropertiesWithCompletionHandler { properties in
-            os_log(.default, "The extension received a message (%@) from a script injected into (%@) with userInfo (%@)", messageName, String(describing: properties?.url), userInfo ?? [:])
-        }
-    }
-
-    override func toolbarItemClicked(in window: SFSafariWindow) {
-        os_log(.default, "The extension's toolbar item was clicked")
-    }
-
-    override func validateToolbarItem(in window: SFSafariWindow, validationHandler: @escaping ((Bool, String) -> Void)) {
-        validationHandler(true, "")
-    }
-
-    override func popoverViewController() -> SFSafariExtensionViewController {
-        return SafariExtensionViewController.shared
-    }
-
+    
 }
