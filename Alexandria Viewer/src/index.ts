eb021f5faf156f37b55f9fdc47413b268365353b
Laurin Brandner <mail@laurinbrandner.ch>
2023-10-29 21:18:28 +0100

    Simple PDF viewer

--- Alexandria Viewer/src/index.ts
+++ Alexandria Viewer/src/index.ts
@@ -1,24 +1,75 @@
-const pdfjs = require("pdfjs-dist");
-const pdfjsWorker = require("pdfjs-dist/build/pdf.worker.entry");
+import * as pdfLib from "pdfjs-dist";
+import * as pdfViewer from "pdfjs-dist/web/pdf_viewer";
+// const xref = require("crossref");
 
-pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
+pdfLib.GlobalWorkerOptions.workerSrc = require("pdfjs-dist/build/pdf.worker.entry");
+
+class PDFViewer {
+
+    url: String
+    pdf: pdfLib.PDFDocumentProxy | undefined
+    container: HTMLDivElement;
+
+    constructor(url: String, container: HTMLDivElement) {
+        this.url = url;
+        this.container = container;
+    }
+
+    load() {
+        pdfLib.getDocument(this.url).promise.then((pdf: pdfLib.PDFDocumentProxy) => {
+            this.pdf = pdf;
+
+            const eventBus = new pdfViewer.EventBus();
+            const linkService = new pdfViewer.PDFLinkService({
+                eventBus,
+                externalLinkRel: "noopener noreferrer nofollow",
+                externalLinkTarget: pdfViewer.LinkTarget.BLANK
+            });	
+            const viewer = new pdfViewer.PDFViewer({
+                container: this.container,
+                eventBus,
+                linkService,
+                l10n: pdfViewer.NullL10n,
+                textLayerMode: 2
+            });
+
+            linkService.setViewer(viewer);
+
+            eventBus.on("pagesinit", function () {
+                viewer.currentScaleValue = "page-width";
+            });
+
+            viewer.setDocument(this.pdf);
+            linkService.setDocument(this.pdf);
+        });
+    }
+
+}
 
 window.onload = () => {
-    if (document.contentType == "application/pdf") {
-        document.body.innerHTML = body();
-    }
-    
+    if (document.contentType != "application/pdf") return;
+
+    const container = prepareBody();    
     const url = window.location.href;
-    pdfjs.getDocument(url).promise.then(function (pdf: any) {
-        pdf.getMetadata().then(function(stuff: any) {
-            console.log(stuff); // Metadata object here
-        }).catch(function(err: any) {
-           console.log('Error getting meta data');
-           console.log(err);
-        });
-    });
+    const viewer = new PDFViewer(url, container);
+    viewer.load();
+
+    // const title = "Pollux: Co-adaptive Cluster Scheduling for Goodput-Optimized Deep Learning";
+    // const query = { query: title };
+    // xref.works(query, (err: any, objs: any, nextOpts: any, done: boolean) => {
+    //     console.log(objs);
+    // });
 };
 
-function body() {
-    return "<p>Aboobsato Impsum</p>";
+function prepareBody() {
+    const container = document.createElement("div");
+    container.setAttribute("id", "pdf-container");
+
+    const viewer = document.createElement("div");
+    viewer.setAttribute("class", "pdfViewer");
+    container.appendChild(viewer);
+
+    document.body.replaceChildren(container);
+
+    return container;
 }
--- Alexandria Viewer/webpack.config.js
+++ Alexandria Viewer/webpack.config.js
@@ -24,10 +24,7 @@ const config = {
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
                 type: 'asset',
-            },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+            }
         ],
     },
     resolve: {
