1bfd7d6095b13dce2b036a95fdbdf6b9b44612fd
Laurin Brandner <mail@laurinbrandner.ch>
2023-11-04 18:29:29 +0100

    Set document title

--- Alexandria Viewer/src/index.ts
+++ Alexandria Viewer/src/index.ts
@@ -22,82 +22,124 @@ class PDFViewer {
     url: String
     pdf: pl.PDFDocumentProxy | undefined
     container: HTMLDivElement;
+    viewer: pv.PDFViewer | undefined;
 
     constructor(url: String, container: HTMLDivElement) {
         this.url = url;
         this.container = container;
     }
 
-    load() {
-        pl.getDocument(this.url).promise.then((pdf: pl.PDFDocumentProxy) => {
-            this.pdf = pdf;
 
-            const eventBus = new pv.EventBus();
-            const linkService = new pv.PDFLinkService({
-                eventBus,
-                externalLinkRel: "noopener noreferrer nofollow",
-                externalLinkTarget: pv.LinkTarget.BLANK
-            });	
-            const viewer = new pv.PDFViewer({
-                container: this.container,
-                eventBus,
-                linkService,
-                l10n: pv.NullL10n
-            });
+    async load() {
+        this.pdf = await pl.getDocument(this.url).promise;
 
-            linkService.setViewer(viewer);
-
-            eventBus.on("pagesinit", () => {
-                viewer.currentScaleValue = "page-width";
-            });
-
-            eventBus.on('textlayerrendered', () => {
-                const layer = document.getElementsByClassName("textLayer")[0];
-
-                var text = "";
-                var idx: number[] = [];
-
-                for (const elem of layer.children) {
-                    idx.push(text.length);
-                    text += elem.textContent;
-                }
-
-                pdf.getPage(1).then(page => {
-                    extractTitleFromPage(page).then(value => {
-                        const title = value[0];
-
-                        const k = text.indexOf(title);
-                        if (k > 0) {
-                            var elems = [];
-                            
-                            idx.forEach((i, j) => {
-                                if (i > k + title.length) return;
-
-                                if (i >= k && i < k + title.length) {
-
-                                    const query = encodeURIComponent(title);
-                                    const url = `https://scholar.google.com/scholar?q=${query}`
-
-                                    const span = layer.children[j];
-                                    const a = document.createElement("a");
-                                    a.setAttribute("href", url);
-                                    a.setAttribute("target", "_blank");
-                                    a.innerHTML = span.outerHTML;
-
-                                    layer.insertBefore(a, span);
-                                    span.remove();
-                                }
-                            });
-                        }
-
-                    });
-                });
-            });
-
-            viewer.setDocument(this.pdf);
-            linkService.setDocument(this.pdf);
+        const eventBus = new pv.EventBus();
+        const linkService = new pv.PDFLinkService({
+            eventBus,
+            externalLinkRel: "noopener noreferrer nofollow",
+            externalLinkTarget: pv.LinkTarget.BLANK
+        });	
+        this.viewer = new pv.PDFViewer({
+            container: this.container,
+            eventBus,
+            linkService,
+            l10n: pv.NullL10n
         });
+
+        this.viewer.setDocument(this.pdf);
+        linkService.setViewer(this.viewer);
+        linkService.setDocument(this.pdf);
+
+        eventBus.on("pagesinit", () => {
+            this.viewer.currentScaleValue = "page-width";
+        });
+
+        eventBus.on('textlayerrendered', () => {
+            
+        });
+
+        const title = await this._getPDFTitle();
+        this._setDocumentTitle(title);
     }
+
+    _loadNodeTree() {
+        const layer = document.getElementsByClassName("textLayer")[0];
+
+            var text = "";
+            var idx: number[] = [];
+
+            for (const elem of layer.children) {
+                idx.push(text.length);
+                text += elem.textContent;
+            }
+
+                // pdf.getPage(1).then(page => {
+                //     extractTitleFromPage(page).then(value => {
+                //         const title = value[0];
+
+                //         const k = text.indexOf(title);
+                //         if (k > 0) {
+                //             var elems = [];
+                            
+                //             idx.forEach((i, j) => {
+                //                 if (i > k + title.length) return;
+
+                //                 if (i >= k && i < k + title.length) {
+
+                //                     const query = encodeURIComponent(title);
+                //                     const url = `https://scholar.google.com/scholar?q=${query}`
+
+                //                     const span = layer.children[j];
+                //                     const a = document.createElement("a");
+                //                     a.setAttribute("href", url);
+                //                     a.setAttribute("target", "_blank");
+                //                     a.innerHTML = span.outerHTML;
+
+                //                     layer.insertBefore(a, span);
+                //                     span.remove();
+                //                 }
+                //             });
+                //         }
+
+                //     });
+                // });
+    }
+
+    async _getPDFTitle(): Promise<string> {
+        const page = await this.pdf.getPage(1);
+        const textContent = await page.getTextContent();
+    
+        var title = "";
+        var maxHeight = 0;
+    
+        for (const elem of textContent.items) {
+            const item = elem as TextItem;
+    
+            if (item.height > maxHeight) {
+                maxHeight = item.height;
+                title = item.str;
+            }
+            else if (item.height == maxHeight) {
+                title += item.str;
+            }
+        }
+    
+        return title;
+    }
+
+    _setDocumentTitle(text: string) {
+        const title = document.createElement("title");
+        title.innerText = text;
+
+        const head = document.createElement("head");
+        head.appendChild(title);
+        
+        document.body.insertAdjacentElement("beforebegin", head);
+    }
+
+    // addLink(text: string, url: string) {
+        
+    // }
 
 }
 
@@ -125,30 +167,4 @@ function prepareBody(): HTMLDivElement {
     document.body.appendChild(container);
 
     return container;
-}
-
-async function extractTitleFromPage(page: pl.PDFPageProxy): Promise<[string, Array<any>]> {
-    let textContent = await page.getTextContent();
-
-    var title = "";
-    var maxHeight = 0;
-    var rect = [0, 0, 0, 0];
-
-    for (const elem of textContent.items) {
-        const item = elem as TextItem;
-
-        if (item.height > maxHeight) {
-            maxHeight = item.height;
-            title = item.str;
-            rect = [item.transform[4], item.transform[5], item.width, item.height];
-        }
-        else if (item.height == maxHeight) {
-            title += item.str;
-
-            const nr = [item.transform[4], item.transform[5], item.width, item.height];
-            rect = [Math.min(rect[0], nr[0]), Math.min(rect[1], nr[1]), Math.max(rect[2], nr[2]), Math.max(rect[3], nr[3])];
-        }
-    }
-
-    return [title, rect];
 }
