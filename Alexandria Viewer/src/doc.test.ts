e4b1d32f9662d0ca77dd66cc594ec725f839e8dd
Laurin Brandner <mail@laurinbrandner.ch>
2023-12-16 11:57:39 +0100

    More structured tests

--- Alexandria Viewer/src/doc.test.ts
+++ Alexandria Viewer/src/doc.test.ts
@@ -6,7 +6,7 @@ async function loadDocument(url: string): Promise<AcademicDocumentProxy> {
     return new AcademicDocumentProxy(pdf);
 }
 
-it("loads the title", async () => {
+describe("loads the title", () => {
     const cases = {
         "res/lei.pdf": "Tackling Parallelization Challenges of Kernel Network Stack for Container Overlay Networks",
         "res/miano.pdf": "A Framework for eBPF-Based Network Functions in an Era of Microservices",
@@ -14,50 +14,61 @@ it("loads the title", async () => {
     };
 
     for (const [url, expectedTitle] of Object.entries(cases)) {
-        const doc = await loadDocument(url);
-        const title = await doc.loadTitle();
-    
-        expect(title).toBe(expectedTitle);
+        it(url, async () => {
+            const doc = await loadDocument(url);
+            const title = await doc.loadTitle();
+        
+            expect(title).toBe(expectedTitle);
+        });
     }
 });
 
-it("loads all citations", async () => {
+describe("loads all citations", () => {
     const cases = {
         "res/qiao.pdf": [2, ["31", "67", "46", "57"]],
         "res/zhu.pdf": [1, ["2", "2", "1", "31", "38", "4", "18", "30", "39", "4", "18", "3", "14"]],
-        "res/he.pdf": [1, ["1", "16", "4", "9", "10", "12", "6", "2", "20"]]
+        "res/he.pdf": [1, ["1", "16", "4", "9", "10", "12", "6", "2", "20"]],
+        "res/elokda.pdf": [1, ["1", "2", "3", "4", "5", "6", "7", "8", "4", "9", "10", "4", "11"]]
     };
 
     for (const [url, [pageNumber, expectedCitations]] of Object.entries(cases)) {
-        const doc = await loadDocument(url);
+        it(url, async () => {
+            const doc = await loadDocument(url);
 
-        var citations = new Array<string>();
-        for await (const [item, ranges] of doc.iterateCitations(pageNumber as number)) {    
-            expect(ranges.length).toBeGreaterThan(0);
-            for (const [s, e] of ranges) {
-                citations.push(item.str.substring(s, e));
+            var citations = new Array<string>();
+            for await (const [item, ranges] of doc.iterateCitations(pageNumber as number)) {    
+                expect(ranges.length).toBeGreaterThan(0);
+                for (const [s, e] of ranges) {
+                    citations.push(item.str.substring(s, e));
+                }
             }
-        }
-    
-        expect(citations).toEqual(expectedCitations);
+        
+            expect(citations).toEqual(expectedCitations);
+        });
     }
 });
 
-it("loads all references", async () => {
+describe("loads all references", () => {
     const cases = {
-        "res/he.pdf": 33
+        "res/he.pdf": 33,
+        "res/elokda.pdf": 66,
+        "res/lei.pdf": 29,
+        "res/mehra.pdf": 18,
+        "res/miano.pdf": 69
     };
 
     for (const [url, expectedNumReferences] of Object.entries(cases)) {
-        const doc = await loadDocument(url);
-        const refs = await doc.loadReferences();
-    
-        expect(refs).not.toBeNull();
-        expect(refs.size).toEqual(expectedNumReferences);
+        it(url, async () => {
+            const doc = await loadDocument(url);
+            const refs = await doc.loadReferences();
+        
+            expect(refs).not.toBeNull();
+            expect(refs.size).toEqual(expectedNumReferences);
+        });
     }
 });
 
-it("finds the title", async () => {
+describe("finds the title", () => {
     const cases = {
         "res/qiao.pdf": [[1, 2], "Pollux: Co-adaptive Cluster Scheduling for Goodput-Optimized Deep Learning"],
         "res/zhu.pdf": [[1], "Dissecting Service Mesh Overheads"],
@@ -65,24 +76,26 @@ it("finds the title", async () => {
     };
 
     for (const [url, [pageNumbers, expectedTitle]] of Object.entries(cases)) {
-        const doc = await loadDocument(url);
+        it(url, async () => {
+            const doc = await loadDocument(url);
 
-        for (const pageNumber of pageNumbers) {
-            for await (const occurrences of doc.iterateOccurences(pageNumber as number, expectedTitle as string)) {    
-                var title = "";
-                for (const [item, s, e] of occurrences) {
-                    expect(s).toBeGreaterThanOrEqual(0);
-                    expect(s).toBeLessThan(e);
-    
-                    title += item.str.substring(s, e);
-                    if (item.hasEOL) {
-                        title += " ";
+            for (const pageNumber of pageNumbers) {
+                for await (const occurrences of doc.iterateOccurences(pageNumber as number, expectedTitle as string)) {    
+                    var title = "";
+                    for (const [item, s, e] of occurrences) {
+                        expect(s).toBeGreaterThanOrEqual(0);
+                        expect(s).toBeLessThan(e);
+        
+                        title += item.str.substring(s, e);
+                        if (item.hasEOL) {
+                            title += " ";
+                        }
                     }
-                }
-                title = title.trim();
+                    title = title.trim();
 
-                expect(title).toBe(expectedTitle);
+                    expect(title).toBe(expectedTitle);
+                }
             }
-        }
+        });
     }
 });
